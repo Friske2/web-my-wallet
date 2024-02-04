b5270fd6331c8874da483c41dcb65d1a64765067
jellopy <parnupat.j@gmail.com>
2024-02-04 17:45:30 +0700

    init page add wallet

--- src/views/AddWallet.tsx
+++ src/views/AddWallet.tsx
@@ -1,9 +1,28 @@
 import React from 'react';
-
+import { useNavigate } from 'react-router-dom'
 function PageAddWallet() {
+  const history = useNavigate();
+  function toPageHome() {
+    history('/');
+  }
   return (
     <div>
-      Add Wallet
+     <div className='mb-2'>
+        <h4>เพิ่มกระเป๋าเงิน</h4>
+      </div>
+      <form>
+        <div className="mb-3">
+          <label htmlFor="walletName" className="form-label">Name</label>
+          <input type="email" className="form-control" id="walletName" />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="walletAmount" className="form-label">Amount</label>
+          <input type="email" className="form-control" id="walletAmount" />
+        </div>
+        <div className="d-grid gap-2 my-2">
+          <button onClick={toPageHome} type="submit" className="btn btn-primary">Save</button>
+        </div>
+      </form>
     </div>
   );
 }
